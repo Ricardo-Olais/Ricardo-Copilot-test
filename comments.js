c673684799364efa317e992bb52598b17665ab72
Ricardo-Olais <92885715+Ricardo-Olais@users.noreply.github.com>
2025-02-27 19:42:09 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -2,22 +2,36 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var querystring = require('querystring');
 var comments = [];
-http.createServer(function(request, response) {
-    var urlParsed = url.parse(request.url, true);
-    console.log(urlParsed);
-
-    if (urlParsed.pathname == '/echo' && urlParsed.query.text) {
-        response.setHeader('Cache-control', 'no-cache');
-        response.end(urlParsed.query.text);
-    } else if (urlParsed.pathname == '/comments') {
-        if (urlParsed.query.comment) {
-            comments.push(urlParsed.query.comment);
-        }
-        response.setHeader('Cache-control', 'no-cache');
-        response.end(JSON.stringify(comments));
-    } else {
-        response.statusCode = 404;
-        response.end('Page not found');
+var server = http.createServer(function(req, res){
+    //parse the url
+    var urlObj = url.parse(req.url, true);
+    var pathname = urlObj.pathname;
+    if(pathname === '/'){
+        fs.readFile('./index.html', function(err, data){
+            if(err){
+                console.log(err);
+            }else{
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.end(data);
+            }
+        });
+    }else if(pathname === '/comment'){
+        //get the query string
+        var query = urlObj.query;
+        comments.push(query.comment);
+        res.end(JSON.stringify(comments));
+    }else{
+        fs.readFile('.' + pathname, function(err, data){
+            if(err){
+                res.writeHead(404, {'Content-Type': 'text/html'});
+                res.end('<h1>404 Not Found</h1>');
+            }else{
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.end(data);
+            }
+        });
     }
-}).listen(3000)
+});
+server.listen(3000, '
