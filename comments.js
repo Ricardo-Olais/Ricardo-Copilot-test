c210d9cd0edd89e89e5e33c243057809c6ab0fab
Ricardo-Olais <92885715+Ricardo-Olais@users.noreply.github.com>
2025-02-28 16:51:24 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,37 +1,30 @@
-//create web server
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var querystring = require('querystring');
-var comments = [];
-var server = http.createServer(function(req, res){
-    //parse the url
-    var urlObj = url.parse(req.url, true);
-    var pathname = urlObj.pathname;
-    if(pathname === '/'){
-        fs.readFile('./index.html', function(err, data){
-            if(err){
-                console.log(err);
-            }else{
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.end(data);
-            }
-        });
-    }else if(pathname === '/comment'){
-        //get the query string
-        var query = urlObj.query;
-        comments.push(query.comment);
-        res.end(JSON.stringify(comments));
-    }else{
-        fs.readFile('.' + pathname, function(err, data){
-            if(err){
-                res.writeHead(404, {'Content-Type': 'text/html'});
-                res.end('<h1>404 Not Found</h1>');
-            }else{
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.end(data);
-            }
-        });
-    }
+//create web server using express
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const bodyParser = require('body-parser');
+const port = 3000;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Database setup
+const comments = [];
+
+// Routes
+app.get('/comments', (req, res) => {
+    res.json(comments);
 });
-server.listen(3000, '
+
+app.post('/comments', (req, res) => {
+    const comment = req.body;
+    comments.push(comment);
+    res.status(201).json(comment);
+});
+
+// Start server
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
